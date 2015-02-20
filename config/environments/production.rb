02f44a9b8ce3768734a97d70d063356026f5d71e
Christine Murabito <christine.murabito@gmail.com>
2015-02-20 16:48:04 -0500

    Added devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  #Enter real url when in production for devise
+  #config.action_mailer.default_url_options = { :host => 'http://pinteresting-commits.herokuapp.com/' }
+  
+  
 end
